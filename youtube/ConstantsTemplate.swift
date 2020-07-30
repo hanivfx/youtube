1bef89a30612cf9240d5c1d6913575121a26862d
Hani AlYousif <hani.alyousif@gmail.com>
2020-07-30 10:53:35 +0100

    Added Video object and Model class

---
+++ youtube/ConstantsTemplate.swift
@@ -0,0 +1,22 @@
+//
+//  ConstantsTemplate.swift
+//  youtube
+//
+//  Created by Hani AlYousif on 30/07/2020.
+//  Copyright © 2020 Hani AlYousif. All rights reserved.
+//
+
+/*
+    Add your own keys below and uncomment the code
+ */
+
+import Foundation
+
+
+/* struct Constants {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    
+} */
---
+++ youtube/Video.swift
@@ -0,0 +1,19 @@
+//
+//  Video.swift
+//  youtube
+//
+//  Created by Hani AlYousif on 30/07/2020.
+//  Copyright © 2020 Hani AlYousif. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
